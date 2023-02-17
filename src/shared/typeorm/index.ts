cf31015bfd387e86742a3a932de3f5f3503aa7df
Thiago Lopes <sawadalopes@gmail.com>
2023-02-17 06:48:05 -0300

    project creation + project configuration

---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,12 @@
+import { DataSource } from 'typeorm';
+
+export const appDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'apivendas',
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+});
+appDataSource.initialize();
